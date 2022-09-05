c2f1b9c522646c6d35adc9c4ddc4dfaf9f54bf81
pinkul1234 <pinkul5555@gmail.com>
2022-09-06 02:06:27 +0530

    Initial commit

---
+++ src/test/java/com/bridgelabz/netflixzuulapigatewayserverapplication/NetflixZuulApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.netflixzuulapigatewayserverapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
